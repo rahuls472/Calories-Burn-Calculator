2489e6e023c055c03fb7d1e596c1d2b0b858381c
Rahul Singh <rahulsingh07d@gmail.com>
2025-05-26 09:31:58 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -15,7 +15,7 @@ X_scalar = Scaler.fit_transform(X)
 
 # Train model
 X_train, X_test, y_train, y_test = train_test_split(X_scalar, y, test_size=0.2, random_state=42)
-knn = KNeighborsRegressor(n_neighbors=13)
+knn = KNeighborsRegressor(n_neighbors=13,weights = 'distance')
 knn.fit(X_train, y_train)
 
 #Save Model
